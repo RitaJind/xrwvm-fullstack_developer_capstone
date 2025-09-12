028118a531c5c08edbac9e5eaee9dd9a8cda41fd
RitaJind <rjindal20@yahoo.com>
2025-09-12 13:25:15 -0400

    Update 3views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -67,8 +67,10 @@ def registration(request):
         )
     if not username_exist:
         user = User.objects.create_user(
-            username=username, first_name=first_name,
-            last_name=last_name, password=password,
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
             email=email
         )
         login(request, user)
