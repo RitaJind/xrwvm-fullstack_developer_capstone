5824aefaa078b0e363db5b8f7ba3a26f7a5417c8
RitaJind <rjindal20@yahoo.com>
2025-09-12 13:11:29 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -120,8 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-]
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
