edee3612e37eadf8e6aebff09d640193392a20d0
RitaJind <rjindal20@yahoo.com>
2025-09-12 14:38:28 -0400

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -36,6 +36,12 @@ urlpatterns = [
         'postreview/<int:dealer_id>',
         TemplateView.as_view(template_name="index.html")
     ),
-    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path(
+        'dealer/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path(
+        'postreview/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
