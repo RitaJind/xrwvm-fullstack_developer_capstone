08bba16ddb271c2b8241149531f62c69f73b2d83
Rita Jindal <rjindal20@yahoo.com>
2025-09-22 07:59:38 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -18,23 +18,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =\
-    'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = [
     'localhost',
+    '127.0.0.1',
     'https://rjindal-8000.'
     'theianext-1-labs-prod-misc-tools-us-east-0.'
-    'proxy.cognitiveclass.ai'
+    'proxy.cognitiveclass.ai',
+    '.railway.app'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://rjindal-8000.theianext-1-labs-prod-misc-tools-us-east-0.'
     'proxy.cognitiveclass.ai',
     'https://rjindal-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
-    'proxy.cognitiveclass.ai'
+    'proxy.cognitiveclass.ai',
+    'https://*.railway.app'
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
