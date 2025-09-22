df826fb5b90c18205af34ed0805b1594b4b192a2
Rita Jindal <rjindal20@yahoo.com>
2025-09-22 08:08:18 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,14 +30,14 @@ ALLOWED_HOSTS = [
     'https://rjindal-8000.'
     'theianext-1-labs-prod-misc-tools-us-east-0.'
     'proxy.cognitiveclass.ai',
-    '.railway.app'
+    '.onrender.com'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://rjindal-8000.theianext-1-labs-prod-misc-tools-us-east-0.'
     'proxy.cognitiveclass.ai',
     'https://rjindal-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
     'proxy.cognitiveclass.ai',
-    'https://*.railway.app'
+    'https://*.onrender.com'
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -89,10 +90,11 @@ WSGI_APPLICATION = 'djangoproj.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
